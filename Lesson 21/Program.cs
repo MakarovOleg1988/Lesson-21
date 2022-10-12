915183fce13a69604f849761d6db2c2dc4e3a99d
MakarovOleg1988 <makarov.oleg1988@yandex.ru>
2022-10-12 08:02:09 +0300

    Добавьте файлы проекта.

---
+++ Lesson 21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace Lesson_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _cl = new ReflectionMethod();
+            var _type = _cl.GetType();
+            _type = typeof(ReflectionMethod);
+
+            var _fields = _type.GetFields();
+
+            var _fieldName = _fields[0].Name;
+
+            var _field = _type.GetField("_fieldint");
+            _field.SetValue(_cl, 12);
+        }
+    }
+
+    public class ReflectionMethod
+    {
+        private int _fieldint;
+        protected short _fieldshort;
+        public long _fieldlong;
+
+        private char _protectedchar { get; set; }
+        protected string _protectedshort { get; set; }
+        public DateTime __publicDateTime { get; set; }
+
+        private void _setInt(int t) { }
+        protected object _getObject => null;
+        public TimeSpan _getTimeSpan() => TimeSpan.MaxValue; 
+    }
+}
