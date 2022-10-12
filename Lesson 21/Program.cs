8b51dbda5fd77151e8b09badcf113cd5d3989c46
MakarovOleg1988 <makarov.oleg1988@yandex.ru>
2022-10-12 11:20:11 +0530

    Дополнительный код

--- Lesson 21/Program.cs
+++ Lesson 21/Program.cs
@@ -8,15 +8,28 @@ namespace Lesson_21
         static void Main(string[] args)
         {
             var _cl = new ReflectionMethod();
+            var _cl1 = new ReflectionMethod();
             var _type = _cl.GetType();
             _type = typeof(ReflectionMethod);
 
-            var _fields = _type.GetFields();
+            var _fields = _type.GetProperty("_protectedString", BindingFlags.NonPublic | BindingFlags.Instance);
+            _fields.SetValue(_cl, "this is test");
 
-            var _fieldName = _fields[0].Name;
+            Console.WriteLine(_fields.GetValue(_cl));
 
-            var _field = _type.GetField("_fieldint");
+            var _fields1 = _type.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var _field = _type.GetField("_fieldint", BindingFlags.NonPublic | BindingFlags.Instance);
             _field.SetValue(_cl, 12);
+
+            var _method = _fields.GetMethod;
+
+            Console.WriteLine(_field.GetValue(_cl));
+
+            var _field1 = _type.GetField("_fieldlong", BindingFlags.NonPublic | BindingFlags.Instance);
+            _field1.SetValue(_cl1, (long)1124);
+
+            Console.WriteLine(_field.GetValue(_cl1));
         }
     }
 
@@ -24,10 +37,10 @@ namespace Lesson_21
     {
         private int _fieldint;
         protected short _fieldshort;
-        public long _fieldlong;
+        protected long _fieldlong;
 
         private char _protectedchar { get; set; }
-        protected string _protectedshort { get; set; }
+        protected string _protectedString { get; set; }
         public DateTime __publicDateTime { get; set; }
 
         private void _setInt(int t) { }
